8e494908b1f81625f47af4ba7a2cb4c139d9f19c
Yusef Taher <yuseftaher2008@hotmail.com>
2026-07-01 16:09:18 +0300

    cloud database

--- db.js
+++ db.js
@@ -3,11 +3,7 @@ import pkg from 'pg'
 const { Pool } = pkg
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 })
-
 export default pool
